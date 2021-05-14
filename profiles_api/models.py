3302c7d48db528548c63da5f4e99d05b0abd03d0
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2021-05-14 15:44:15 +0700

    Adding base register user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Email address field is required')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_super_user(self, email, name, password=None):
+        user = self.create_user(email, name, password)
+        user.is_admin = True
+        user.save()
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
