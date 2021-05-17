c5c13fde60499d918fd97ceae92d796914fa313f
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2021-05-17 20:08:22 +0700

    Adding view set for profiles

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,5 @@
 from .serializers import HelloSerializer
-from rest_framework import status
+from rest_framework import status, viewsets, serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -26,11 +26,46 @@ class HelloApiView(APIView):
 
         return Response({'code': self.code, 'message': self.message, 'data': data})
 
-    def put(self, request):
+    def put(self, request, pk=None):
         return Response({'code': self.code, 'message': self.message, 'data': 'PUT'})
 
-    def patch(self, request):
+    def patch(self, request, pk=None):
         return Response({'code': self.code, 'message': self.message, 'data': 'PATCH'})
 
-    def delete(self, request):
+    def delete(self, request, pk=None):
+        return Response({'code': self.code, 'message': self.message, 'data': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    code = 200
+    message = "success"
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        view = ['Hello', 'World']
+        return Response({'code': self.code, 'message': self.message, 'data': view})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+        else:
+            self.message = 'error'
+            self.code = status.HTTP_400_BAD_REQUEST
+            message = serializer.errors
+
+        return Response({'code': self.code, 'message': self.message, 'data': message})
+
+    def retrieve(self, request, pk=None):
+        return Response({'code': self.code, 'message': self.message, 'data': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'code': self.code, 'message': self.message, 'data': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'code': self.code, 'message': self.message, 'data': 'PATCH'})
+
+    def destroy(self, request, pk=None):
         return Response({'code': self.code, 'message': self.message, 'data': 'DELETE'})
